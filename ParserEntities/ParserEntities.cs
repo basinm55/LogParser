7d32d5015357e3edbdae4eff334e5a4c2b2ed1ef
basinm <basinm@gmail.com>
2020-06-04 07:22:31 +0300

    v.0.0.0.0

--- ParserEntities/ParserEntities.cs
+++ ParserEntities/ParserEntities.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Dynamic;
 
-namespace ParserEntities
+namespace ParserAppEntities
 {
     public class Enums
     {
@@ -28,36 +28,57 @@ namespace ParserEntities
             Decimal = 1,
             Hex = 2,
             DateTime = 3
-        }      
+        }
     }
 
 
     public class ParserObject
     {
-        public string Name { get; set; }
-        public DateTime Timestamp { get; set; }
-
+        public string ObjectClass { get; private set; }
+        public DateTime CreationTimeStamp { get; set; }
         public bool IsExists { get; set; }
-
-        public ExpandoObject DynObject { get; }
+        private dynamic _dynObject { get; }
 
         private IDictionary<string, object> _dynObjectDictionary;
 
 
-
         //C'tor
-        public ParserObject()
+        public ParserObject(string objectClass, DateTime timeStamp)
         {
-            //dynamic DynObject = new ExpandoObject();
-            //var dictionary = (IDictionary<string, object>)DynObject;
-            //var dictionary = (IDictionary<string, object>)person;
-            //_dynObjectDictionary = (IDictionary<string, object>)DynObject;
-            //dictionary.Add("Name", "Filip");
-            //dictionary.Add("Age", 24);
-
-            dynamic person = new ExpandoObject();
-            var dictionary = (IDictionary<string, object>)person;
+            _dynObject = new ExpandoObject();
+            _dynObjectDictionary = (IDictionary<string, object>)_dynObject;
+            ObjectClass = objectClass;
+            CreationTimeStamp = timeStamp;
         }
 
+        public void AddDynProperty(string propertyName, object propertyValue)
+        {
+            _dynObjectDictionary.Add(propertyName, propertyValue);
+        }
+
+        public void RemoveDynProperty(string propertyName, object propertyValue)
+        {
+            _dynObjectDictionary.Remove(propertyName);
+        }
+
+        public void ClearDynProperties()
+        {
+            _dynObjectDictionary.Clear();
+        }
+
+        public object GetDynPropertyValue(string propertyName)
+        {
+            _dynObjectDictionary.TryGetValue(propertyName, out object value);
+            return value;
+
+        }
+
+        public void SetDynPropertyValue(string propertyName, object value)
+        {
+            if (_dynObjectDictionary.ContainsKey(propertyName))
+                _dynObjectDictionary[propertyName] = value;
+            else
+                throw new Exception(string.Format("Property '{0}' not found", propertyName));
+        }
     }
 }
--- ParserEntities/Properties/AssemblyInfo.cs
+++ ParserEntities/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("454e9fce-74d7-4547-b91f-429e96b8ad89")]
+[assembly: Guid("e0d23060-68ad-427b-a437-0d64c65c9115")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.0.0")]
-[assembly: AssemblyFileVersion("0.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
