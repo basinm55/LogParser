317500f8fabf7f6e17169fbf604dd8646fe1275a
basinm <basinm@gmail.com>
2020-06-04 07:03:44 +0300

    v.0.0.0.0

--- ParserEntities1/Properties/AssemblyInfo.cs
+++ ParserEntities1/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ParserEntities1")]
+[assembly: AssemblyTitle("ParserEntities")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ParserEntities1")]
+[assembly: AssemblyProduct("ParserEntities")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
