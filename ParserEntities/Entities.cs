aecd5739a617958127733d3045e4609d5e20b0c9
basinm <basinm@gmail.com>
2020-06-04 07:32:52 +0300

    v.0.0.0.0

--- ParserEntities/Entities.cs
+++ ParserEntities/Entities.cs
@@ -2,36 +2,8 @@
 using System.Collections.Generic;
 using System.Dynamic;
 
-namespace ParserAppEntities
-{
-    public class Enums
-    {
-
-        public enum Action
-        {
-            New = 0,
-            Locate = 1,
-            Assign = 2,
-            Drop = 3,
-            Delete = 4
-        }
-
-        public enum Shape
-        {
-            Rectangle = 0,
-            Circle = 1
-        }
-
-        public enum PropertyDataType
-        {
-            String = 0,
-            Decimal = 1,
-            Hex = 2,
-            DateTime = 3
-        }
-    }
-
-
+namespace Entities
+{ 
     public class ParserObject
     {
         public string ObjectClass { get; private set; }
---
+++ ParserEntities/Enums.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class Enums
+    {
+
+        public enum Action
+        {
+            New = 0,
+            Locate = 1,
+            Assign = 2,
+            Drop = 3,
+            Delete = 4
+        }
+
+        public enum Shape
+        {
+            Rectangle = 0,
+            Circle = 1
+        }
+
+        public enum PropertyDataType
+        {
+            String = 0,
+            Decimal = 1,
+            Hex = 2,
+            DateTime = 3
+        }
+    }
+}
