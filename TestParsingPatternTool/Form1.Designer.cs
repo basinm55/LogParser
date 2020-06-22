a9eeb2b44a67038e897b4e03f90ebf1359c5979a
basinm <basinm@gmail.com>
2020-06-22 22:31:36 +0300

    v.0.0.0.7

---
+++ TestParsingPatternTool/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace TestParsingPatternTool
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtBoxLogEntry = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtBoxPattern = new System.Windows.Forms.TextBox();
+            this.btnTest = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.dGVResults = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dGVResults)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtBoxLogEntry
+            // 
+            this.txtBoxLogEntry.Location = new System.Drawing.Point(29, 33);
+            this.txtBoxLogEntry.Multiline = true;
+            this.txtBoxLogEntry.Name = "txtBoxLogEntry";
+            this.txtBoxLogEntry.Size = new System.Drawing.Size(878, 89);
+            this.txtBoxLogEntry.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Single Log Entry:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(26, 150);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(109, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Parsing pattern string:";
+            // 
+            // txtBoxPattern
+            // 
+            this.txtBoxPattern.Location = new System.Drawing.Point(29, 174);
+            this.txtBoxPattern.Multiline = true;
+            this.txtBoxPattern.Name = "txtBoxPattern";
+            this.txtBoxPattern.Size = new System.Drawing.Size(878, 89);
+            this.txtBoxPattern.TabIndex = 2;
+            // 
+            // btnTest
+            // 
+            this.btnTest.Location = new System.Drawing.Point(29, 274);
+            this.btnTest.Name = "btnTest";
+            this.btnTest.Size = new System.Drawing.Size(84, 66);
+            this.btnTest.TabIndex = 4;
+            this.btnTest.Text = "Test";
+            this.btnTest.UseVisualStyleBackColor = true;
+            this.btnTest.Click += new System.EventHandler(this.btnTest_Click);
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResult.Location = new System.Drawing.Point(128, 302);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(0, 13);
+            this.lblResult.TabIndex = 7;
+            // 
+            // dGVResults
+            // 
+            this.dGVResults.AllowUserToAddRows = false;
+            this.dGVResults.AllowUserToDeleteRows = false;
+            this.dGVResults.AllowUserToResizeRows = false;
+            this.dGVResults.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.DisplayedCells;
+            this.dGVResults.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dGVResults.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.dGVResults.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dGVResults.ColumnHeadersVisible = false;
+            this.dGVResults.Enabled = false;
+            this.dGVResults.Location = new System.Drawing.Point(29, 354);
+            this.dGVResults.MultiSelect = false;
+            this.dGVResults.Name = "dGVResults";
+            this.dGVResults.ReadOnly = true;
+            this.dGVResults.RowHeadersVisible = false;
+            this.dGVResults.Size = new System.Drawing.Size(878, 137);
+            this.dGVResults.TabIndex = 5;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(929, 506);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.dGVResults);
+            this.Controls.Add(this.btnTest);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtBoxPattern);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtBoxLogEntry);
+            this.Name = "frmMain";
+            this.Text = "Test Parsing Pattern Tool";
+            ((System.ComponentModel.ISupportInitialize)(this.dGVResults)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtBoxLogEntry;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtBoxPattern;
+        private System.Windows.Forms.Button btnTest;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.DataGridView dGVResults;
+    }
+}
+
---
+++ TestParsingPatternTool/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Helpers;
+
+namespace TestParsingPatternTool
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+            //txtBoxLogEntry.Text = "[7]0000.0000::05/31/2020-16:43:13.927 [jtucxip]CUsbipRequest::CUsbipRequest: type R dev FFFFC30247334410 -> FFFFC3023C1F74F0";
+            //txtBoxPattern.Text = "[% *1d]% *4c.% *4c::% s[% *7s]CUsbipRequest::CUsbipRequest: type % s dev % s-> % s";
+            //dGVResults.RowsDefaultCellStyle.SelectionBackColor = dGVResults.RowsDefaultCellStyle.BackColor.IsEmpty ? System.Drawing.Color.White : dGVResults.RowsDefaultCellStyle.BackColor;
+            //dGVResults.RowsDefaultCellStyle.SelectionForeColor = dGVResults.RowsDefaultCellStyle.ForeColor.IsEmpty ? System.Drawing.Color.Black : dGVResults.RowsDefaultCellStyle.ForeColor;
+        }
+
+        private void btnTest_Click(object sender, EventArgs e)
+        {
+            ScanFormatted sf = new ScanFormatted();
+            sf.Parse(txtBoxLogEntry.Text.Trim(), txtBoxPattern.Text.Trim());            
+            var isSuccess = IsParsingSuccessful(sf.Results);
+
+            var ds = new List<KeyValuePair<int, string>>();
+            int i = 0;
+            foreach (var res in sf.Results)
+            {
+                ds.Add(new KeyValuePair<int, string>(i, (string)res));
+                i++;
+            }
+            if (!isSuccess)
+            {
+                dGVResults.ForeColor = Color.Red;
+                lblResult.BackColor = Color.Red;
+                lblResult.ForeColor = Color.White;
+                lblResult.Text = "Error";
+            }
+            else
+            {
+                dGVResults.ForeColor = Color.Green;
+                lblResult.BackColor = Color.Green;
+                lblResult.ForeColor = Color.White;
+                lblResult.Text = "OK";
+            }
+         
+            dGVResults.DataSource = ds;
+            dGVResults.ClearSelection();
+        }
+
+        private bool IsParsingSuccessful(List<object> results)
+        {
+            if (results.Count == 0)
+                return false;
+
+            var percentCount = txtBoxPattern.Text.Count(c => c == '%');
+            var droppedPercentCount = txtBoxPattern.Text.Split(new string[] { "%*" }, StringSplitOptions.None).Length - 1;
+            if (results.Count != percentCount - droppedPercentCount)
+                return false;
+
+            return true;
+        }
+    }
+}
